bbcd15989d9e17890613081dd6ac03b49b4e4331
izaii <koejelek12345@gmail.com>
2022-09-08 12:31:22 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -31,18 +31,18 @@ module.exports = {
         global.img = 'https://telegra.ph/file/dffdaf0e8d59ad104e526.jpg'
         global.fla = pickRandom(global.flaaa)
         global.namabot = conn.user.name
-        global.packname = '© RaaBotz-MD'
-        global.author = 'Made By Claraa © 2022'
-        global.packname2 = 'Made With'
-        global.author2 = 'Raa Bot By Claraa © 2022'
-        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Claraa˚'
-        global.wm2 = '©𝙍𝙖𝙖-𝘽𝙤𝙩𝙯-𝙈𝘿' + ' ' + 'By 𝐂𝐥𝐚𝐫𝐚𝐚'
-        global.wm = '© 𝙍𝙖𝙖-𝘽𝙤𝙩𝙯--𝙈𝘿'
+        global.packname = '© FaizBot-MD'
+        global.author = 'Made By Arif © 2022'
+        global.packname2 = 'Made With Faiz'
+        global.author2 = 'Faiz botZ© 2022'
+        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Arif'
+        global.wm2 = '©Faiz-Botz-𝙈𝘿' + ' ' + 'By Arif'
+        global.wm = '© Faiz-Botz-𝙈𝘿'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'By Claraa'
+        global.colong2 = 'By Faiz'
         global.kontak2 = [
-        ['62895604670507', 'Claraa', 'Creator Bot', 'Clarz939@support.com', true],
-        ['79217628068', 'Raa Botz-MD', 'Bot WhatsApp', 'Just Bot', true]
+        ['6285865462352', 'Arif', 'Creator Bot', 'Clarz939@support.com', true],
+        ['62895808168700', 'FaizMD', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
